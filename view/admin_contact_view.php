82e4e701c81d2861d00fe1fca35972ce5021b400
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-02-10 07:57:44 +0900

    Update admin_contact_view.php
    a 태그의 링크를 수정하고 코드 정리 작업을 했습니다.

--- view/admin_contact_view.php
+++ view/admin_contact_view.php
@@ -23,28 +23,28 @@
                 <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                     <div class="alert" style="padding-left:50px; padding-right:50px">
                         <br>
-                        <?
-                        $no=$row->no;
-						$type=$row->type;
-                        $q_name=$row->q_name;
-						$c_name=$row->c_name;
-                        $c_email=$row->c_email;
-                        $c_phone=$row->c_phone;
-                        $q_bigo=$row->q_bigo;
+                        <?php 
+							$no=$row->no;
+							$type=$row->type;
+							$q_name=$row->q_name;
+							$c_name=$row->c_name;
+							$c_email=$row->c_email;
+							$c_phone=$row->c_phone;
+							$q_bigo=$row->q_bigo;
                         ?>
 							<div class="row" style="padding-left:10px">
 								<div class="col-lg-2" style="padding-top:20px">
 									<label style="font-size: 18px; margin:0; font-family: 'Noto Sans KR', sans-serif;">&nbsp상담유형</label>
-									<input style="width:150px" type="text" name="type" id="type" size="20" maxlength="20" value="<?=$type?>" oninput="check_id()" class="form-control" disabled>
+									<input style="width:150px" type="text" name="type" id="type" size="20" maxlength="20" value="<?php echo $type?>" oninput="check_id()" class="form-control" disabled>
 								</div>
 								<div class="col-lg-2" style="padding-top:20px">
 									<label style="font-size: 18px; margin:0; font-family: 'Noto Sans KR', sans-serif;">&nbsp제목</label>
-									<input style="width:250px" type="text" name="type" id="type" size="20" value="<?=$q_name?>" oninput="check_id()" class="form-control" disabled>
+									<input style="width:250px" type="text" name="type" id="type" size="20" value="<?php echo $q_name?>" oninput="check_id()" class="form-control" disabled>
 								</div>
 							</div>
                             <div class="col-12" style="padding-top:20px">
                                 <label style="font-size: 18px; margin:0; font-family: 'Noto Sans KR', sans-serif;">&nbsp문의내용</label>
-                                <textarea style="width:500px;" type="text" name="c_bigo" id="c_bigo" rows="6" class="form-control" disabled><?=$q_bigo?></textarea>
+                                <textarea style="width:500px;" type="text" name="c_bigo" id="c_bigo" rows="6" class="form-control" disabled><?php echo $q_bigo?></textarea>
                             </div>
 							
 							<br>
@@ -53,22 +53,22 @@
 							
 							 <div class="col-6" style="padding-top:20px">
                                 <label style="font-size: 18px; margin:0; font-family: 'Noto Sans KR', sans-serif;">&nbsp성명</label>
-                                <input style="width:150px" type="text" name="c_name" id="c_name" size="20" maxlength="20" value="<?=$c_name?>" oninput="check_id()" class="form-control" disabled>
+                                <input style="width:150px" type="text" name="c_name" id="c_name" size="20" maxlength="20" value="<?php echo $c_name?>" oninput="check_id()" class="form-control" disabled>
                             </div>
-
+						
 							<div class="col-6" style="padding-top:20px">
-                            <label style="font-size: 18px; margin:0; font-family: 'Noto Sans KR', sans-serif;">&nbsp이메일</label>
-                            <input style="width:300px" type="email" name="c_email" id="c_email" size="20" maxlength="20" value="<?=$c_email?>" class="form-control" disabled>
+								<label style="font-size: 18px; margin:0; font-family: 'Noto Sans KR', sans-serif;">&nbsp이메일</label>
+								<input style="width:300px" type="email" name="c_email" id="c_email" size="20" maxlength="20" value="<?php echo $c_email?>" class="form-control" disabled>
 							</div>
                             
                             <div class="col-6" style="padding-top:20px">
 								<label style="font-size: 18px; margin:0; font-family: 'Noto Sans KR', sans-serif;">&nbsp휴대전화</label>
-								<input style="width:300px" type="tel" name="c_phone" id="c_phone" size="20" maxlength="20" value="<?=$c_phone?>" class="form-control" disabled>
+								<input style="width:300px" type="tel" name="c_phone" id="c_phone" size="20" maxlength="20" value="<?php echo $c_phone?>" class="form-control" disabled>
 							</div>
                         </div>
 							
 						<div class="col" align="right">
-							<a href="/~team1/contact/del/?no=<?=$no?>" onClick="return confirm('삭제할까요?');" class="btn btn-danger btn-icon-split">
+							<a href="/contact/del/?no=<?php echo $no;?>" onClick="return confirm('삭제할까요?');" class="btn btn-danger btn-icon-split">
 								<span class="icon text-white-50"><i class="fas fa-trash"></i></span>
 								<span class="text">삭제하기</span>
 							</a>
@@ -78,7 +78,6 @@
             </div>
         </div>
     </div>
-
 </div>
 <!-- /.container-fluid -->
 
