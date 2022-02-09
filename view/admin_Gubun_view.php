6c13327dd0599dbb691cf123d1ec41ce9d29bf85
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-02-10 07:20:09 +0900

    Update admin_Gubun_view.php
    a 태그 링크를 수정 하고 코드 정리 작업을 했습니다.

--- view/admin_Gubun_view.php
+++ view/admin_Gubun_view.php
@@ -1,4 +1,3 @@
-
 <!-- Begin Page Content -->
 <div class="container-fluid">
 
@@ -10,26 +9,26 @@
         <div class="card-body">
             <div class="table-responsive">
                 <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                    <?
-                    $no=$row->no;
-                    $name=$row->name;
+                    <?php
+			    $no=$row->no;
+			    $name=$row->name;
                     ?>
                     <div class="alert" style="padding-left:50px; padding-right:50px">
                         <br>
                         <div class="form-group row">
-                            <div class="col-lg-6" style="padding-top:10px">
-                                <label style="font-size: 18px; margin:0; font-family: 'Noto Sans KR', sans-serif;">&nbspNo</label>
-                                <label style="font-size: 13px; margin:0; font-family: 'Noto Sans KR', sans-serif; color:red">&nbsp※수정이 불가능 합니다.※</label>
-                                <input style="width:150px" type="text" name="no" id="no" size="20" maxlength="20" value="<?=$no?>" oninput="check_id()" class="form-control" disabled>
-                            </div>
-                            <div class="col-lg-6" style="padding-top:10px">
-                                <label style="font-size: 18px; margin:0; font-family: 'Noto Sans KR', sans-serif;">&nbsp구분 이름</label>
-                                <input style="width:150px" type="text" name="name" id="name" size="20" maxlength="20" value="<?=$name?>" oninput="check_id()" class="form-control" disabled>
-                            </div>
-							<div class="col-lg-6"></div>
-                            <div class="col-lg-6" align="left" style="padding-top:20px; padding-right: 50px">
-                                <a href="/~team1/adminroomgubun/edit/?no=<?=$no?>" style="width:200px; margin-right:10px" class="btn btn-block btn-primary" >수정하기</a>
-                            </div>
+				<div class="col-lg-6" style="padding-top:10px">
+					<label style="font-size: 18px; margin:0; font-family: 'Noto Sans KR', sans-serif;">&nbspNo</label>
+					<label style="font-size: 13px; margin:0; font-family: 'Noto Sans KR', sans-serif; color:red">&nbsp※수정이 불가능 합니다.※</label>
+					<input style="width:150px" type="text" name="no" id="no" size="20" maxlength="20" value="<?php echo $no?>" oninput="check_id()" class="form-control" disabled>
+				</div>
+				<div class="col-lg-6" style="padding-top:10px">
+					<label style="font-size: 18px; margin:0; font-family: 'Noto Sans KR', sans-serif;">&nbsp구분 이름</label>
+					<input style="width:150px" type="text" name="name" id="name" size="20" maxlength="20" value="<?php echo $name?>" oninput="check_id()" class="form-control" disabled>
+				</div>
+			<div class="col-lg-6"></div>
+                        	<div class="col-lg-6" align="left" style="padding-top:20px; padding-right: 50px">
+                                	<a href="/adminroomgubun/edit/?no=<?=$no?>" style="width:200px; margin-right:10px" class="btn btn-block btn-primary" >수정하기</a>
+                        	</div>
                         </div>
                     </div>
                 </table>
