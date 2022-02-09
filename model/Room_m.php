cbcdbfac5dc90042b888f19da3dadcd96a47c255
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-02-09 10:05:44 +0900

    Update Room_m.php
    코드 정리 작업을 했습니다.

--- model/Room_m.php
+++ model/Room_m.php
@@ -1,28 +1,33 @@
 <?
-    class Room_m extends CI_Model     // 모델 클래스 선언
-    {
-		public function getresult($no){
-			$sql="select * from room where no like $no";
-			return $this->db->query($sql)->row();
-		}
-		public function getreplenum($no){
-			$sql="select * from room_reple where room_no like $no";
-			return $this->db->query($sql)->num_rows();
-		}
-		public function getreple($no){
-			$sql="select * from room_reple where room_no like $no order by no DESC";
-			return $this->db->query($sql)->result();
-		}
-		public function getroom(){
-			$sql="select * from room";
-			return $this->db->query($sql)->result();
-		}
-		public function deletereple($no){
-			$sql="delete from room_reple where no like $no";
-			return  $this->db->query($sql);
-		}
-		public function insertreple($row){
-			return $this->db->insert("room_reple",$row);
-		}
-    }
+class Room_m extends CI_Model{
+	public function getresult($no)
+	{
+		$sql="select * from room where no like $no";
+		return $this->db->query($sql)->row();
+	}
+	public function getreplenum($no)
+	{
+		$sql="select * from room_reple where room_no like $no";
+		return $this->db->query($sql)->num_rows();
+	}
+	public function getreple($no)
+	{
+		$sql="select * from room_reple where room_no like $no order by no DESC";
+		return $this->db->query($sql)->result();
+	}
+	public function getroom()
+	{
+		$sql="select * from room";
+		return $this->db->query($sql)->result();
+	}
+	public function deletereple($no)
+	{
+		$sql="delete from room_reple where no like $no";
+		return  $this->db->query($sql);
+	}
+	public function insertreple($row)
+	{
+		return $this->db->insert("room_reple",$row);
+	}
+}
 ?>
