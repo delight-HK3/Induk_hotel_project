1f514fdb7f9d834ab52a1f99348fec0c3004b630
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-02-09 10:06:49 +0900

    Update Service_m.php
    코드 정리 작업을 했습니다.

--- model/Service_m.php
+++ model/Service_m.php
@@ -1,28 +1,33 @@
 <?
-	class Service_m extends CI_Model     // 모델 클래스 선언
-    {
-		public function getservice(){
-			$sql="select * from service";   
-			return $this->db->query($sql)->result();
-		}
-		public function getrow($no){
-			$sql="select * from service where no like $no";   
-			return $this->db->query($sql)->row();
-		}
-		public function getreplenum($no){
-			$sql="select * from service_reple where service_no like $no";
-			return $this->db->query($sql)->num_rows();
-		}
-		public function getreple($no){
-			$sql="select * from service_reple where service_no like $no order by no DESC";
-			return $this->db->query($sql)->result();
-		}
-		public function deletereple($no){
-			$sql="delete from service_reple where no like $no";
-			return  $this->db->query($sql);
-		}
-		public function insertreple($row){
-			return $this->db->insert("service_reple",$row);
-		}
+class Service_m extends CI_Model{
+	public function getservice()
+	{
+		$sql="select * from service";   
+		return $this->db->query($sql)->result();
 	}
+	public function getrow($no)
+	{
+		$sql="select * from service where no like $no";   
+		return $this->db->query($sql)->row();
+	}
+	public function getreplenum($no)
+	{
+		$sql="select * from service_reple where service_no like $no";
+		return $this->db->query($sql)->num_rows();
+	}
+	public function getreple($no)
+	{
+		$sql="select * from service_reple where service_no like $no order by no DESC";
+		return $this->db->query($sql)->result();
+	}
+	public function deletereple($no)
+	{
+		$sql="delete from service_reple where no like $no";
+		return  $this->db->query($sql);
+	}
+	public function insertreple($row)
+	{
+		return $this->db->insert("service_reple",$row);
+	}
+}
 ?>
