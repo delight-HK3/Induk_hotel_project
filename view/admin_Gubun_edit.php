6e99582c2bde715e3eaf756630994bbca90a737e
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-02-10 07:15:47 +0900

    Update admin_Gubun_edit.php
    form, a, script 태그의 링크를 수정하고 코드 정리 작업을 했습니다.

--- view/admin_Gubun_edit.php
+++ view/admin_Gubun_edit.php
@@ -9,34 +9,34 @@
         <div class="card-body">
             <div class="table-responsive">
                 <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                    <?
-                    $no=$row->no;
-                    $name=$row->name;
+                    <?php
+                    	$no=$row->no;
+                    	$name=$row->name;
                     ?>
                     <div class="alert" style="padding-left:50px; padding-right:50px">
                         <br>
-                        <form id="GubunEdit" method="post" action="/~team1/adminroomgubun/goedit/?no=<?=$no?>">
+                        <form id="GubunEdit" method="post" action="/adminroomgubun/goedit/?no=<?=$no?>">
                             <div class="form-group row">
                                 <div class="col-lg-6" style="padding-top:10px">
-                                    <label style="font-size: 18px; margin:0; font-family: 'Noto Sans KR', sans-serif;">&nbspNo</label>
-                                    <label style="font-size: 13px; margin:0; font-family: 'Noto Sans KR', sans-serif; color:red">&nbsp※수정이 불가능 합니다.※</label>
-                                    <input style="width:150px" type="text" name="eno" id="no" size="20" maxlength="20" value="<?=$no?>"  class="form-control" readonly="readonly">
+                                	<label style="font-size: 18px; margin:0; font-family: 'Noto Sans KR', sans-serif;">&nbspNo</label>
+                                	<label style="font-size: 13px; margin:0; font-family: 'Noto Sans KR', sans-serif; color:red">&nbsp※수정이 불가능 합니다.※</label>
+                                	<input style="width:150px" type="text" name="eno" id="no" size="20" maxlength="20" value="<?php echo $no?>"  class="form-control" readonly="readonly">
                                 </div>
                                 <div class="col-lg-6" style="padding-top:10px">
-                                    <label style="font-size: 18px; margin:0; font-family: 'Noto Sans KR', sans-serif;">&nbsp구분 이름</label>
-									<label style="font-size: 13px; margin:0; font-family: 'Noto Sans KR', sans-serif; color:red">&nbsp※필수사항 입니다.※</label>
-                                    <input style="width:150px" type="text" name="ename" id="name" size="20" maxlength="20" value="<?=$name?>" class="form-control" >
+                                	<label style="font-size: 18px; margin:0; font-family: 'Noto Sans KR', sans-serif;">&nbsp구분 이름</label>
+					<label style="font-size: 13px; margin:0; font-family: 'Noto Sans KR', sans-serif; color:red">&nbsp※필수사항 입니다.※</label>
+                                	<input style="width:150px" type="text" name="ename" id="name" size="20" maxlength="20" value="<?=$name?>" class="form-control" >
                                 </div>
                                 <div class="col-lg-6" style="padding-top:20px"></div>
                                 <div class="col-lg-6 row" style="padding-top:20px; padding-left:25px" align="left">
-									<a href="/~team1/adminroomgubun/del/?no=<?=$no?>" onClick="return confirm('삭제할까요?');" class="btn btn-danger btn-icon-split">
-										<span class="icon text-white-50"><i class="fas fa-trash"></i></span>
-										<span class="text">삭제하기</span>
-									</a>&nbsp&nbsp&nbsp
-									<a href="#" onclick="gubuncheck()" class="btn btn-success btn-icon-split">
-										<span class="icon text-white-50"><i class="fas fa-check"></i></span>
-										<span class="text">저장하기</span>
-									</a>
+					<a href="/adminroomgubun/del/?no=<?php echo $no?>" onClick="return confirm('삭제할까요?');" class="btn btn-danger btn-icon-split">
+						<span class="icon text-white-50"><i class="fas fa-trash"></i></span>
+						<span class="text">삭제하기</span>
+					</a>&nbsp&nbsp&nbsp
+					<a href="#" onclick="gubuncheck()" class="btn btn-success btn-icon-split">
+						<span class="icon text-white-50"><i class="fas fa-check"></i></span>
+						<span class="text">저장하기</span>
+					</a>
                                 </div>
                             </div>
                         </form>
@@ -51,11 +51,10 @@
 
 </div>
 <!-- End of Main Content -->
-<script src="/~team1/my/js/jquery-3.5.1.min.js"></script>
+<script src="/my/js/jquery-3.5.1.min.js"></script>
 <script>
 	function gubuncheck(){
 		var name = $("#name").val();
-
 		if(name == ""){
 			alert("구분명이 없습니다.");
 			$("#name").focus();
