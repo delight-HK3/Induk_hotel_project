41f210fdb94e6c2606208c4da70c5c2fc338a8a3
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-02-09 10:03:30 +0900

    Update Reservation_m.php
    코드 정리 작업을 했습니다.

--- model/Reservation_m.php
+++ model/Reservation_m.php
@@ -1,27 +1,31 @@
 <?
-    class Reservation_m extends CI_Model     // 모델 클래스 선언
-    {
-		public function getgubun(){
-			$sql="select * from room_gubun";    
-			return $this->db->query($sql)->result();
-		}
-        public function getroom(){
-			$sql="select room.*,
-					room_gubun.name as gubun_name from 
-					room left join room_gubun on 
-					room.gubun_no=room_gubun.no";   
-			return $this->db->query($sql)->result();
-		}
-		public function getselectroom($room){
-			$sql="select * from room where name like '%$room%'";
-			return $this->db->query($sql)->row();
-		}
-		public function getmember($no){
-			$sql="select * from member where no like $no";
-			return $this->db->query($sql)->row();
-		}
-		public function insertrow($row){
-			return $this->db->insert("jangburoom",$row);
-		}
-    }
+class Reservation_m extends CI_Model{
+	public function getgubun()
+	{
+		$sql="select * from room_gubun";    
+		return $this->db->query($sql)->result();
+	}
+        public function getroom()
+	{
+		$sql="select room.*,
+			room_gubun.name as gubun_name from 
+			room left join room_gubun on 
+			room.gubun_no=room_gubun.no";   
+		return $this->db->query($sql)->result();
+	}
+	public function getselectroom($room)
+	{
+		$sql="select * from room where name like '%$room%'";
+		return $this->db->query($sql)->row();
+	}
+	public function getmember($no)
+	{
+		$sql="select * from member where no like $no";
+		return $this->db->query($sql)->row();
+	}
+	public function insertrow($row)
+	{
+		return $this->db->insert("jangburoom",$row);
+	}
+}
 ?>
