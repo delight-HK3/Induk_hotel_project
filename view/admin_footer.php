f84c0625447e0e78131125c06b8b30b62aef6ddf
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-02-10 08:02:26 +0900

    Update admin_footer.php
    a, css, script의 경로를 수정하고 코드 정리 작업을 했습니다.

--- view/admin_footer.php
+++ view/admin_footer.php
@@ -1,48 +1,45 @@
-<!-- Footer -->
-<footer class="sticky-footer bg-white">
-    <div class="container my-auto">
-        <div class="copyright text-center my-auto">
-            <span>Copyright &copy; Your Website 2021</span>
-        </div>
-    </div>
-</footer>
-<!-- End of Footer -->
-
-</div>
-<!-- End of Content Wrapper -->
-
-</div>
-<!-- End of Page Wrapper -->
-
-<!-- Scroll to Top Button-->
-<a class="scroll-to-top rounded" href="#page-top">
-    <i class="fas fa-angle-up"></i>
-</a>
-
-<!-- Logout Modal-->
-<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-     aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">×</span>
-                </button>
+                    <!-- Footer -->
+                    <footer class="sticky-footer bg-white">
+                        <div class="container my-auto">
+                            <div class="copyright text-center my-auto">
+                                <span>Copyright &copy; Your Website 2021</span>
+                            </div>
+                        </div>
+                    </footer>
+                    <!-- End of Footer -->
+                </div>
+            <!-- End of Content Wrapper -->
             </div>
-            <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-            <div class="modal-footer">
-                <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                <a class="btn btn-primary" href="/~team1/admin/clientlogout">Logout</a>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- Custom styles for this template-->
-<link href="/~team1/my/css/sb-admin-2.css" rel="stylesheet">
-<!-- Custom scripts for all pages-->
-<script src="/~team1/my/js/sb-admin-2.min.js"></script>
-</body>
+            <!-- End of Page Wrapper -->
 
+            <!-- Scroll to Top Button-->
+            <a class="scroll-to-top rounded" href="#page-top">
+                <i class="fas fa-angle-up"></i>
+            </a>
+
+            <!-- Logout Modal-->
+            <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+                 aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                            <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">×</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                        <div class="modal-footer">
+                            <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                            <a class="btn btn-primary" href="/admin/clientlogout">Logout</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <!-- Custom styles for this template-->
+        <link href="/my/css/sb-admin-2.css" rel="stylesheet">
+        <!-- Custom scripts for all pages-->
+        <script src="/my/js/sb-admin-2.min.js"></script>
+    </body>
 </html>
 
