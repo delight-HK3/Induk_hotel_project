7c4231de8870f408b6c505c1be67984decc499b3
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-02-09 10:04:48 +0900

    Update Restaurant_m.php
    코드 정리 작업을 했습니다.

--- model/Restaurant_m.php
+++ model/Restaurant_m.php
@@ -1,35 +1,42 @@
 <?
-    class Restaurant_m extends CI_Model     // 모델 클래스 선언
-    {
-		public function getresult($no){
-			$sql="select * from restaurant where no like $no";
-			return $this->db->query($sql)->row();
-		}
-		public function getreplenum($no){
-			$sql="select * from restaurant_reple where restaurant_no like $no";
-			return $this->db->query($sql)->num_rows();
-		}
-		public function getreple($no){
-			$sql="select * from restaurant_reple where restaurant_no like $no order by no DESC";
-			return $this->db->query($sql)->result();
-		}
-		public function getrestaurant(){
-			$sql="select * from restaurant";
-			return $this->db->query($sql)->result();
-		}
-		public function getmember(){
-			$sql="select * from member";
-			return $this->db->query($sql)->row();
-		}
-		public function insertrow($row){
-			return $this->db->insert("jangbumeal",$row);
-		}
-		public function deletereple($no){
-			$sql="delete from restaurant_reple where no like $no";
-			return  $this->db->query($sql);
-		}
-		public function insertreple($row){
-			return $this->db->insert("restaurant_reple",$row);
-		}
+class Restaurant_m extends CI_Model    {
+	public function getresult($no)
+	{
+		$sql="select * from restaurant where no like $no";
+		return $this->db->query($sql)->row();
+	}
+	public function getreplenum($no)
+	{
+		$sql="select * from restaurant_reple where restaurant_no like $no";
+		return $this->db->query($sql)->num_rows();
+	}
+	public function getreple($no)
+	{
+		$sql="select * from restaurant_reple where restaurant_no like $no order by no DESC";
+		return $this->db->query($sql)->result();
+	}
+	public function getrestaurant()
+	{
+		$sql="select * from restaurant";
+		return $this->db->query($sql)->result();
+	}
+	public function getmember()
+	{
+		$sql="select * from member";
+		return $this->db->query($sql)->row();
+	}
+	public function insertrow($row)
+	{
+		return $this->db->insert("jangbumeal",$row);
+	}
+	public function deletereple($no)
+	{
+		$sql="delete from restaurant_reple where no like $no";
+		return  $this->db->query($sql);
+	}
+	public function insertreple($row)
+	{
+		return $this->db->insert("restaurant_reple",$row);
+	}
     }
 ?>
