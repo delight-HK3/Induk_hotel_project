080e55bee02cb72865af8ea973cf6b20ea0c3d37
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-02-11 07:32:01 +0900

    Update admin_jrestaurant_list.php
    a 태그의 링크 수정 및 코드 정리 작업을 했습니다.

--- view/admin_jrestaurant_list.php
+++ view/admin_jrestaurant_list.php
@@ -1,4 +1,3 @@
-
 <!-- Begin Page Content -->
 <div class="container-fluid">
 
@@ -12,52 +11,52 @@
             <div class="table-responsive">
                 <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                     <thead>
-                    <tr>
-						<th>예약번호</th>
-                        <th>레스토랑 이름</th>
-                        <th>예약자</th>
-                        <th>어른 수</th>
-                        <th>아이 수</th>
-						<th>예약일자</th>
-                        <th>방문일자</th>
-                        <th>방문시간</th>
-						<th>예약상태</th>
-                    </tr>
+			    <tr>
+				<th>예약번호</th>
+				<th>레스토랑 이름</th>
+				<th>예약자</th>
+				<th>어른 수</th>
+				<th>아이 수</th>
+				<th>예약일자</th>
+				<th>방문일자</th>
+				<th>방문시간</th>
+				<th>예약상태</th>
+		    	   </tr>
                     </thead>
                     <tbody>
-                    <?
-                    foreach ($list as $row)                             // 연관배열 list를 row를 통해 출력한다.
-                    {
-                        $no=$row->no;
-						$reservation_no=$row->reservation_no;
-                        $adult_num=$row->adult_num;
-                        $child_num=$row->child_num;
-						$reservation_date=$row->reservation_date;
-                        $meal_day=$row->meal_day;
-                        $meal_time=$row->meal_time;
-						$state=$row->state;
-                        ?>
-                        <tr>
-							<td><a href="/~team1/ajrestaurant/view/?no=<?=$no?>"><?=$reservation_no; ?></a></td>
-                            <td><?=$row->restaurant_name; ?></td>
-                            <td><?=$row->member_name; ?></td>
-                            <td><?=$adult_num; ?> 명</td>
-                            <td><?=$child_num; ?> 명</td>
-							<td><?=$reservation_date?></td>
-                            <td><?=$meal_day; ?></td>
-                            <td><?=$meal_time; ?></td>
-							<?
-								if($state == 1){
-									echo("<td style='color:blue'>예약 완료</td>");
-								}
-								else{
-									echo("<td style='color:red'>예약 취소</td>");
-								}
-							?>
-                        </tr>
-                        <?
-                    }
-                    ?>
+			    <?php 
+				    foreach ($list as $row)// 연관배열 list를 row를 통해 출력한다.
+				    {
+					$no=$row->no;
+					$reservation_no=$row->reservation_no;
+					$adult_num=$row->adult_num;
+					$child_num=$row->child_num;
+					$reservation_date=$row->reservation_date;
+					$meal_day=$row->meal_day;
+					$meal_time=$row->meal_time;
+					$state=$row->state;
+			    ?>
+				<tr>
+					<td><a href="/ajrestaurant/view/?no=<?php echo $no?>"><?php echo $reservation_no; ?></a></td>
+					<td><?php echo $row->restaurant_name; ?></td>
+					<td><?php echo $row->member_name; ?></td>
+					<td><?php echo $adult_num; ?> 명</td>
+					<td><?php echo $child_num; ?> 명</td>
+					<td><?php echo $reservation_date?></td>
+					<td><?php echo $meal_day; ?></td>
+					<td><?php echo $meal_time; ?></td>
+					<?
+						if($state == 1){
+							echo("<td style='color:blue'>예약 완료</td>");
+						}
+						else{
+							echo("<td style='color:red'>예약 취소</td>");
+						}
+					?>
+				</tr>
+			    <?
+			    	}
+			    ?>
                     </tbody>
                 </table>
             </div>
