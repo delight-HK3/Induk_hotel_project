9cd4f60baa15adbd96a1057695de761e293753c5
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-02-12 10:16:55 +0900

    Update admin_service_add.php
    a 태그의 링크 수정 및 코드 정리 작업을 했습니다.

--- view/admin_service_add.php
+++ view/admin_service_add.php
@@ -26,42 +26,42 @@
                 <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                     <div class="alert" style="padding-left:50px; padding-right:50px">
                         <br>
-                        <form id="serviceAdd" method="post" enctype="multipart/form-data" action="/~team1/adminservice/add" >
+                        <form id="serviceAdd" method="post" enctype="multipart/form-data" action="/adminservice/add" >
                             <div class="col-6" style="padding-top:20px">
                                 <label style="font-size: 18px; margin:0; font-family: 'Noto Sans KR', sans-serif;">&nbsp이름</label>
-								<label style="font-size: 13px; margin:0; font-family: 'Noto Sans KR', sans-serif; color:red">&nbsp※필수사항 입니다.※</label>
+				<label style="font-size: 13px; margin:0; font-family: 'Noto Sans KR', sans-serif; color:red">&nbsp※필수사항 입니다.※</label>
                                 <input style="width:300px" type="text" name="name" id="name" size="20" value="" oninput="check_id()" class="form-control">
                             </div>
                             <div class="col-6" style="padding-top:20px">
                                 <label style="font-size: 18px; margin:0; font-family: 'Noto Sans KR', sans-serif;">&nbsp가격</label>
-								<label style="font-size: 13px; margin:0; font-family: 'Noto Sans KR', sans-serif; color:red">&nbsp※필수사항 입니다.※</label>
+				<label style="font-size: 13px; margin:0; font-family: 'Noto Sans KR', sans-serif; color:red">&nbsp※필수사항 입니다.※</label>
                                 <input style="width:300px" type="text" name="price" id="price" size="20" maxlength="20" value="" oninput="check_id()" class="form-control" >
                             </div>
                             
                             <div class="col-6" style="padding-top:20px">
                                 <label style="font-size: 18px; margin:0; font-family: 'Noto Sans KR', sans-serif;">&nbsp여는 시간</label>
-								<label style="font-size: 13px; margin:0; font-family: 'Noto Sans KR', sans-serif; color:red">&nbsp※필수사항 입니다.※</label>
+				<label style="font-size: 13px; margin:0; font-family: 'Noto Sans KR', sans-serif; color:red">&nbsp※필수사항 입니다.※</label>
                                 <input style="width:300px" type="time" name="opentime" id="opentime" min="6:00:00" max="11:00:00" value="" oninput="check_id()" class="form-control">
                             </div>
                             <div class="col-6" style="padding-top:20px">
                                 <label style="font-size: 18px; margin:0; font-family: 'Noto Sans KR', sans-serif;">&nbsp닫는 시간</label>
-								<label style="font-size: 13px; margin:0; font-family: 'Noto Sans KR', sans-serif; color:red">&nbsp※필수사항 입니다.※</label>
+				<label style="font-size: 13px; margin:0; font-family: 'Noto Sans KR', sans-serif; color:red">&nbsp※필수사항 입니다.※</label>
                                 <input style="width:300px" type="time" name="closetime" id="closetime" min="13:00:00" max="15:00:00" value="" oninput="check_id()" class="form-control">
                             </div>
-							<div class="row" style="padding-left:10px;">
-								<div class="col-lg-4" style="padding-top:20px">
-									<label style="font-size: 18px; margin:0; font-family: 'Noto Sans KR', sans-serif;">&nbsp설명</label>
-									<textarea style="width:400px;" type="text" name="explanation" id="explanation" rows="4" class="form-control"></textarea>
-								</div>
-								<div class="col-lg-4" style="padding-top:20px">
-									<label style="font-size: 18px; margin:0; font-family: 'Noto Sans KR', sans-serif;">&nbsp세부 설명</label>
-									<textarea style="width:400px;" type="text" name="insertbigo" id="insertbigo" rows="4" class="form-control"></textarea>
-								</div>
-								<div class="col-lg-4" style="padding-top:20px">
-									<label style="font-size: 18px; margin:0; font-family: 'Noto Sans KR', sans-serif;">&nbsp비고</label>
-									<textarea style="width:400px;" type="text" name="bego" id="bego" rows="4" class="form-control"></textarea>
-								</div>
-							</div>
+			    <div class="row" style="padding-left:10px;">
+				<div class="col-lg-4" style="padding-top:20px">
+					<label style="font-size: 18px; margin:0; font-family: 'Noto Sans KR', sans-serif;">&nbsp설명</label>
+					<textarea style="width:400px;" type="text" name="explanation" id="explanation" rows="4" class="form-control"></textarea>
+				</div>
+				<div class="col-lg-4" style="padding-top:20px">
+					<label style="font-size: 18px; margin:0; font-family: 'Noto Sans KR', sans-serif;">&nbsp세부 설명</label>
+					<textarea style="width:400px;" type="text" name="insertbigo" id="insertbigo" rows="4" class="form-control"></textarea>
+				</div>
+				<div class="col-lg-4" style="padding-top:20px">
+					<label style="font-size: 18px; margin:0; font-family: 'Noto Sans KR', sans-serif;">&nbsp비고</label>
+					<textarea style="width:400px;" type="text" name="bego" id="bego" rows="4" class="form-control"></textarea>
+				</div>
+			    </div>
 
                             <!-- Collapsable Card Example -->
                             <div class="container" style="padding-top: 50px; padding-left:10px; margin:0px">
@@ -76,9 +76,8 @@
                                         <!-- Card Content - Collapse -->
                                         <div class="collapse show" id="collapseCard1">
                                             <div class="card-body">
-												<input type="file" name="pic1" id="pic1" size = "20" value = "" class="btn btn-primary btn-icon-split">
+						<input type="file" name="pic1" id="pic1" size = "20" value = "" class="btn btn-primary btn-icon-split">
                                             </div>
-                                            
                                         </div>
                                     </div>
                                 </div>
@@ -97,9 +96,8 @@
                                         <!-- Card Content - Collapse -->
                                         <div class="collapse show" id="collapseCard2">
                                             <div class="card-body">
-												<input type="file" name="pic2" id="pic2" size = "20" value = "" class="btn btn-primary btn-icon-split">
+						<input type="file" name="pic2" id="pic2" size = "20" value = "" class="btn btn-primary btn-icon-split">
                                             </div>
-                                            
                                         </div>
                                     </div>
                                 </div>
@@ -118,7 +116,7 @@
                                         <!-- Card Content - Collapse -->
                                         <div class="collapse show" id="collapseCard3">
                                             <div class="card-body">
-												<input type="file" name="pic3" id="pic3" size = "20" value = "" class="btn btn-primary btn-icon-split">
+						<input type="file" name="pic3" id="pic3" size = "20" value = "" class="btn btn-primary btn-icon-split">
                                             </div>
                                         </div>
                                     </div>
@@ -138,7 +136,7 @@
                                         <!-- Card Content - Collapse -->
                                         <div class="collapse show" id="collapseCard4">
                                             <div class="card-body">
-												<input type="file" name="pic4" id="pic4" size = "20" value = "" class="btn btn-primary btn-icon-split">
+						<input type="file" name="pic4" id="pic4" size = "20" value = "" class="btn btn-primary btn-icon-split">
                                             </div>
                                         </div>
                                     </div>
@@ -158,13 +156,11 @@
                                         <!-- Card Content - Collapse -->
                                         <div class="collapse show" id="collapseCard5">
                                             <div class="card-body">
-												<input type="file" name="pic5" id="pic5" size = "20" value = "" class="btn btn-primary btn-icon-split">
+						<input type="file" name="pic5" id="pic5" size = "20" value = "" class="btn btn-primary btn-icon-split">
                                             </div>
-                                            
                                         </div>
                                     </div>
                                 </div>
-
                             </div>
 	
                                <div class="col" align="right" style="padding-top:20px; padding-right:275px">
@@ -188,7 +184,7 @@
 
 </div>
 <!-- End of Main Content -->
-<script src="/~team1/my/js/jquery-3.5.1.min.js"></script>
+<script src="/my/js/jquery-3.5.1.min.js"></script>
 <script>
 	function servicecheck(){
 		var name = $("#name").val();
