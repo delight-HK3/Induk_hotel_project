ae566c5fd33050146dd68dd3552083eb87f08310
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-02-12 10:08:11 +0900

    Update admin_restaurant_edit.php
    form, img, script의 링크 수정 및 코드 정리 작업을 했습니다.

--- view/admin_restaurant_edit.php
+++ view/admin_restaurant_edit.php
@@ -14,43 +14,42 @@
     <!-- DataTales Example -->
     <div class="card shadow border-left-warning mb-4">
         <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-dark"><?=$row->name?> 수정</h6>
+            <h6 class="m-0 font-weight-bold text-dark"><?php echo $row->name?> 수정</h6>
         </div>
         <div class="card-body">
             <div class="table-responsive">
                 <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                     <div class="alert" style="padding-left:50px; padding-right:50px">
                         <br>
-                        <?
-                        $no=$row->no;
-                        $name=$row->name;
-                        $opentime=$row->opentime;
-                        $closetime=$row->closetime;
-                        $explanation=$row->Explanation;
-                        $pic1=$row->image_res1;
-                        $pic2=$row->image_res2;
-                        $pic3=$row->image_res3;
+                        <?php 
+				$no=$row->no;
+				$name=$row->name;
+				$opentime=$row->opentime;
+				$closetime=$row->closetime;
+				$explanation=$row->Explanation;
+				$pic1=$row->image_res1;
+				$pic2=$row->image_res2;
+				$pic3=$row->image_res3;
                         ?>
-                        <form id="restaurantedit" method="post" enctype="multipart/form-data" action="/~team1/adminrestaurant/goedit?no=<?=$row->no?>" >
-                            
+                        <form id="restaurantedit" method="post" enctype="multipart/form-data" action="/adminrestaurant/goedit?no=<?php echo $row->no?>" >
                             <div class="col-6" style="padding-top:10px">
                                 <label style="font-size: 18px; margin:0; font-family: 'Noto Sans KR', sans-serif;">&nbsp이름</label>
-								<label style="font-size: 13px; margin:0; font-family: 'Noto Sans KR', sans-serif; color:red">&nbsp※필수사항 입니다.※</label>
-                                <input style="width:300px" type="text" name="name" id="name" size="20" value="<?=$name?>" oninput="check_id()" class="form-control" >
+				<label style="font-size: 13px; margin:0; font-family: 'Noto Sans KR', sans-serif; color:red">&nbsp※필수사항 입니다.※</label>
+                                <input style="width:300px" type="text" name="name" id="name" size="20" value="<?php echo $name?>" oninput="check_id()" class="form-control" >
                             </div>
                             <div class="col-6" style="padding-top:10px">
                                 <label style="font-size: 18px; margin:0; font-family: 'Noto Sans KR', sans-serif;">&nbsp여는 시간</label>
-								<label style="font-size: 13px; margin:0; font-family: 'Noto Sans KR', sans-serif; color:red">&nbsp※필수사항 입니다.※</label>
-                                <input style="width:300px" type="time" name="opentime" id="opentime" min="06:00:00" max="08:00:00" value="<?=$opentime?>" oninput="check_id()" class="form-control" >
+				<label style="font-size: 13px; margin:0; font-family: 'Noto Sans KR', sans-serif; color:red">&nbsp※필수사항 입니다.※</label>
+                                <input style="width:300px" type="time" name="opentime" id="opentime" min="06:00:00" max="08:00:00" value="<?php echo $opentime?>" oninput="check_id()" class="form-control" >
                             </div>
                             <div class="col-6" style="padding-top:10px">
                                 <label style="font-size: 18px; margin:0; font-family: 'Noto Sans KR', sans-serif;">&nbsp닫는 시간</label>
-								<label style="font-size: 13px; margin:0; font-family: 'Noto Sans KR', sans-serif; color:red">&nbsp※필수사항 입니다.※</label>
-                                <input style="width:300px" type="time" name="closetime" id="closetime" min="13:00:00" max="15:00:00" value="<?=$closetime?>" oninput="check_id()" class="form-control" >
+				<label style="font-size: 13px; margin:0; font-family: 'Noto Sans KR', sans-serif; color:red">&nbsp※필수사항 입니다.※</label>
+                                <input style="width:300px" type="time" name="closetime" id="closetime" min="13:00:00" max="15:00:00" value="<?php echo $closetime?>" oninput="check_id()" class="form-control" >
                             </div>
                             <div class="col-12" style="padding-top:10px">
                                 <label style="font-size: 18px; margin:0; font-family: 'Noto Sans KR', sans-serif;">&nbsp설명</label>
-                                <textarea style="width:500px;" type="text" name="explanation" id="explanation" rows="6" class="form-control" ><?=$explanation?></textarea>
+                                <textarea style="width:500px;" type="text" name="explanation" id="explanation" rows="6" class="form-control" ><?php echo $explanation?></textarea>
                             </div>
 
                             <!-- Collapsable Card Example -->
@@ -66,21 +65,20 @@
                                         <!-- Card Content - Collapse -->
                                         <div class="collapse show" id="collapseCard1">
                                             <div class="card-body">
-                                                <?
+                                                <?php
                                                 if ($pic1)     // 이미지가 있는 경우
-                                                    echo("<img src='/~team1/my/img/restaurant/$pic1' class='d-block w-100' alt='1'>");
+                                                    echo("<img src='/my/img/restaurant/$pic1' class='d-block w-100' alt='1'>");
                                                 else                   // 이미지가 없는 경우
                                                     echo("<img src='' class='d-block w-100' alt='1'>");
                                                 ?><br>
-												<input type="file" name="pic1" id="pic1" size = "20" value = "" class="btn btn-primary btn-icon-split">
+						<input type="file" name="pic1" id="pic1" size = "20" value = "" class="btn btn-primary btn-icon-split">
                                             </div>
 											
                                         </div>
                                     </div>
                                 </div>
 
-                                <div class="item">
-                                </div>
+                                <div class="item"></div>
 
                                 <!-- Collapsable Card Example -->
                                 <div class="item">
@@ -93,13 +91,13 @@
                                         <!-- Card Content - Collapse -->
                                         <div class="collapse show" id="collapseCard2">
                                             <div class="card-body">
-                                                <?
+                                                <?php
                                                 if ($pic2)     // 이미지가 있는 경우
-                                                    echo("<img src='/~team1/my/img/restaurant/$pic2' class='d-block w-100' alt='1'>");
+                                                    echo("<img src='/my/img/restaurant/$pic2' class='d-block w-100' alt='1'>");
                                                 else                   // 이미지가 없는 경우
                                                     echo("<img src='' class='d-block w-100' alt='1'>");
                                                 ?><br> 
-												<input type="file" name="pic2" id="pic2" size = "20" value = "" class="btn btn-primary btn-icon-split">
+						<input type="file" name="pic2" id="pic2" size = "20" value = "" class="btn btn-primary btn-icon-split">
                                             </div>
                                            
                                         </div>
@@ -120,31 +118,30 @@
                                         <!-- Card Content - Collapse -->
                                         <div class="collapse show" id="collapseCard3">
                                             <div class="card-body">
-                                                <?
+                                                <?php
                                                 if ($pic3)     // 이미지가 있는 경우
-                                                    echo("<img src='/~team1/my/img/restaurant/$pic3' class='d-block w-100' alt='1'>");
+                                                    echo("<img src='/my/img/restaurant/$pic3' class='d-block w-100' alt='1'>");
                                                 else                   // 이미지가 없는 경우
                                                     echo("<img src='' class='d-block w-100' alt='1'>");
                                                 ?><br>
-												<input type="file" name="pic3" id="pic3" size = "20" value = "" class="btn btn-primary btn-icon-split">
+						<input type="file" name="pic3" id="pic3" size = "20" value = "" class="btn btn-primary btn-icon-split">
                                             </div>
-                                            
                                         </div>
                                     </div>
-									<div class="col" style="padding:0px;" align="right">
-										<a href="/~team1/adminrestaurant/del/?no=<?=$no?>" onClick="return confirm('삭제할까요?');" class="btn btn-danger btn-icon-split">
-											<span class="icon text-white-50">
-												<i class="fas fa-trash"></i>
-											</span>
-											<span class="text">삭제하기</span>
-										</a>&nbsp&nbsp&nbsp
-										<a href="#" onclick="restaurantcheck()" class="btn btn-success btn-icon-split">
-											<span class="icon text-white-50">
-												<i class="fas fa-check"></i>
-											</span>
-											<span class="text">저장하기</span>
-										</a>
-									</div>
+				    <div class="col" style="padding:0px;" align="right">
+					<a href="/adminrestaurant/del/?no=<?php echo $no?>" onClick="return confirm('삭제할까요?');" class="btn btn-danger btn-icon-split">
+						<span class="icon text-white-50">
+							<i class="fas fa-trash"></i>
+						</span>
+						<span class="text">삭제하기</span>
+					</a>&nbsp&nbsp&nbsp
+					<a href="#" onclick="restaurantcheck()" class="btn btn-success btn-icon-split">
+						<span class="icon text-white-50">
+							<i class="fas fa-check"></i>
+						</span>
+						<span class="text">저장하기</span>
+					</a>
+				    </div>
                                 </div>
                             </div>      
                         </form>
@@ -159,7 +156,7 @@
 
 </div>
 <!-- End of Main Content -->
-<script src="/~team1/my/js/jquery-3.5.1.min.js"></script>
+<script src="/my/js/jquery-3.5.1.min.js"></script>
 <script>
 	function restaurantcheck(){
 		var name = $("#name").val();
