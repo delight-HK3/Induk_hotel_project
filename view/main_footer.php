e43534fcdcbf7a5a1840f3a2bed34c9d4a055180
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-02-15 07:53:46 +0900

    Update main_footer.php
    a, script 태그의 경로를 수정했고 코드 정리 작업을 했습니다.

--- view/main_footer.php
+++ view/main_footer.php
@@ -5,14 +5,9 @@
                     <div class="col-lg-3  col-md-6 col-sm-6">
                         <div class="single-footer-widget">
                             <h6 class="footer_title">Our Address</h6>
-							
-								<p><span class="d-block"><span class="fa fa-map-marker h5 mr-3 text-primary"></span><span>&nbsp;서울특별시 노원구 초안산로 12</span> <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;인덕호텔</span></p>
-								<p><span class="d-block"><span class="fa fa-phone h5 mr-3 text-primary"></span>&nbsp;82-2-2951-7100</span> <span></span></p>
-								<p><span class="d-block"><span class="fa fa-envelope h5 mr-3 text-primary"></span>&nbsp;comso@induk.ac.kr</span> <span></span></p>
-
-
-							
-							
+			    <p><span class="d-block"><span class="fa fa-map-marker h5 mr-3 text-primary"></span><span>&nbsp;서울특별시 노원구 초안산로 12</span> <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;인덕호텔</span></p>
+			    <p><span class="d-block"><span class="fa fa-phone h5 mr-3 text-primary"></span>&nbsp;82-2-2951-7100</span> <span></span></p>
+			    <p><span class="d-block"><span class="fa fa-envelope h5 mr-3 text-primary"></span>&nbsp;comso@induk.ac.kr</span> <span></span></p>		
                         </div>
                     </div>
                     <div class="col-lg-3 col-md-6 col-sm-6">
@@ -21,19 +16,18 @@
                             <div class="row">
                                 <div class="col-4">
                                     <ul class="list_style">
-                                        <li><a href="/~team1/main">Home</a></li>
-                                        <li><a href="/~team1/room">Room</a></li>
-                                        <li><a href="/~team1/restaurant">Restaurant</a></li>
-                                        <li><a href="/~team1/service">Service</a></li> 
+                                        <li><a href="/main">Home</a></li>
+                                        <li><a href="/room">Room</a></li>
+                                        <li><a href="/restaurant">Restaurant</a></li>
+                                        <li><a href="/service">Service</a></li> 
                                     </ul>
                                 </div>
                                 <div class="col-4">
                                     <ul class="list_style">
-                                        <li><a href="/~team1/about">호텔소개</a></li>
-                                        <li><a href="/~team1/about/#about_NUM">연락처</a></li>
-										<li><a href="/~team1/contact">고객문의</a></li>
-										<li><a href="/~team1/about#about_FAQ">FAQ</a></li>
-                                        
+                                        <li><a href="/about">호텔소개</a></li>
+                                        <li><a href="/about/#about_NUM">연락처</a></li>
+					<li><a href="/contact">고객문의</a></li>
+					<li><a href="/about#about_FAQ">FAQ</a></li>
                                     </ul>
                                 </div>										
                             </div>							
@@ -57,112 +51,125 @@
                         <div class="single-footer-widget instafeed">
                             <h6 class="footer_title">attraction</h6> <!--InstaFeed → 주변 주변명소 수정-->
                             <ul class="list_style instafeed d-flex flex-wrap">
-                                <li><a href="https://sema.seoul.go.kr/it/artinfo/northart/getIntro">
-									<img src="/~team1/my/img/instagram/01.jpg" alt=""></a>
-								</li>
+                                <li>
+					<a href="https://sema.seoul.go.kr/it/artinfo/northart/getIntro">
+						<img src="/my/img/instagram/01.jpg" alt="">
+					</a>
+				</li>
 
-                                <li><a href="https://royaltombs.cha.go.kr/multiBbz/selectMultiBbzView.do?id=52&no=52&bbzId=tombs&mn=RT_01_05_03">
-									<img src="/~team1/my/img/instagram/02.jpg" alt=""></a>
-								</li>
+                                <li>
+					<a href="https://royaltombs.cha.go.kr/multiBbz/selectMultiBbzView.do?id=52&no=52&bbzId=tombs&mn=RT_01_05_03">
+						<img src="/my/img/instagram/02.jpg" alt="">
+					</a>
+				</li>
 
-                                <li><a href="https://museum.seoul.go.kr/sulm/information/museInfo.jsp">
-									<img src="/~team1/my/img/instagram/03.jpg" alt=""></a>
-								</li>
+                                <li>
+					<a href="https://museum.seoul.go.kr/sulm/information/museInfo.jsp">
+						<img src="/my/img/instagram/03.jpg" alt="">
+					</a>
+				</li>
 
-                                <li><a href="https://www.nowon.kr/www/info/info1/info1_06/info1_06_08/info1_06_08_01.jsp">
-									<img src="/~team1/my/img/instagram/04.jpg" alt=""></a>
-								</li>
+                                <li>
+					<a href="https://www.nowon.kr/www/info/info1/info1_06/info1_06_08/info1_06_08_01.jsp">
+						<img src="/my/img/instagram/04.jpg" alt="">
+					</a>
+				</li>
 
-                                <li><a href="https://www.nowon.kr/www/info/info4/info4_01/info4_01_12.jsp">
-									<img src="/~team1/my/img/instagram/05.jpg" alt=""></a>
-								</li>
+                                <li>
+					<a href="https://www.nowon.kr/www/info/info4/info4_01/info4_01_12.jsp">
+						<img src="/my/img/instagram/05.jpg" alt="">
+					</a>
+				</li>
 
-                                <li><a href="https://science.seoul.go.kr/tmpl/static/info/use?menuId=2">
-									<img src="/~team1/my/img/instagram/06.jpg" alt=""></a>
-								</li>
+                                <li>
+					<a href="https://science.seoul.go.kr/tmpl/static/info/use?menuId=2">
+						<img src="/my/img/instagram/06.jpg" alt="">
+					</a>
+				</li>
 
-                                <li><a href="https://nowoncosmos.or.kr/page/about/about04.html">
-									<img src="/~team1/my/img/instagram/07.jpg" alt=""></a>
-								</li>
+                                <li>
+					<a href="https://nowoncosmos.or.kr/page/about/about04.html">
+						<img src="/my/img/instagram/07.jpg" alt="">
+					</a>
+				</li>
 
-                                <li><a href="https://www.nowon.kr/math/autocontents/contents/main/113">
-									<img src="/~team1/my/img/instagram/08.jpg" alt=""></a>
-								</li>
+                                <li>
+					<a href="https://www.nowon.kr/math/autocontents/contents/main/113">
+						<img src="/my/img/instagram/08.jpg" alt="">
+					</a>
+				</li>
                             </ul>
                         </div>
                     </div>		
                 </div>
-				<div class="border_line"></div>
-				<div class="row footer-bottom d-flex justify-content-between align-items-center">
-					<p style="padding-left:15px">Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>	</p>
-					<div class="col-lg-4 col-sm-12 footer-social">
-						<a href="https://www.facebook.com/"><i class="fa fa-facebook"></i></a>
-						<a href="https://twitter.com/"><i class="fa fa-twitter"></i></a>
-						<a href="https://www.instagram.com/?hl=ko"><i class="fa fa-instagram"></i></a>
-						<a href="https://goo.gl/maps/H7aWdZ9ifg1prxGx7"><i class="fa fa-map"></i></a>
-					</div>
-				</div>	
+		<div class="border_line"></div>
+		<div class="row footer-bottom d-flex justify-content-between align-items-center">
+			<p style="padding-left:15px">Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>	</p>
+			<div class="col-lg-4 col-sm-12 footer-social">
+				<a href="https://www.facebook.com/"><i class="fa fa-facebook"></i></a>
+				<a href="https://twitter.com/"><i class="fa fa-twitter"></i></a>
+				<a href="https://www.instagram.com/?hl=ko"><i class="fa fa-instagram"></i></a>
+				<a href="https://goo.gl/maps/H7aWdZ9ifg1prxGx7"><i class="fa fa-map"></i></a>
+			</div>
+		</div>	
             </div>
-			<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-			<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-
+	<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+	<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
         </footer>
-		<!--================ End footer Area  =================-->
-        
-        
+	<!--================ End footer Area  =================-->
+
         <!-- Optional JavaScript -->
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 		
-        <script src="/~team1/my/js/popper.js"></script>
-		<script src="/~team1/my/js/bootstrap.js"></script>
-        <script src="/~team1/my/js/bootstrap.min.js"></script>
+        <script src="/my/js/popper.js"></script>
+	<script src="/my/js/bootstrap.js"></script>
+        <script src="/my/js/bootstrap.min.js"></script>
 		
-		<script src="//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js"></script><!--우편번호 찾는 스크립트-->
-        <script src="/~team1/my/vendors/owl-carousel/owl.carousel.min.js"></script>
-        <script src="/~team1/my/js/jquery.ajaxchimp.min.js"></script>
+	<script src="//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js"></script><!--우편번호 찾는 스크립트-->
+        <script src="/my/vendors/owl-carousel/owl.carousel.min.js"></script>
+        <script src="/my/js/jquery.ajaxchimp.min.js"></script>
        
-        <script src="/~team1/my/vendors/bootstrap-datepicker/bootstrap-datetimepicker.min.js"></script>
-		<script src="/~team1/my/js/jquery-ui.min.js"></script>
-        <script src="/~team1/my/vendors/nice-select/js/jquery.nice-select.js"></script>
-		<script src="/~team1/my/js/date.js"></script><!--날짜 관련 스크립트-->
-        <script src="/~team1/my/js/stellar.js"></script>
-		<script src="/~team1/my/js/agree.js"></script><!--회원가입 약관 스크립트-->
-		<script src="/~team1/my/js/mypage_edit.js"></script><!--주소수정 스크립트-->
+        <script src="/my/vendors/bootstrap-datepicker/bootstrap-datetimepicker.min.js"></script>
+	<script src="/my/js/jquery-ui.min.js"></script>
+        <script src="/my/vendors/nice-select/js/jquery.nice-select.js"></script>
+	<script src="/my/js/date.js"></script><!--날짜 관련 스크립트-->
+        <script src="/my/js/stellar.js"></script>
+	<script src="/my/js/agree.js"></script><!--회원가입 약관 스크립트-->
+	<script src="/my/js/mypage_edit.js"></script><!--주소수정 스크립트-->
 
-		<script src="/~team1/my/js/detail_date.js"></script><!--디테일 room 스크립트-->
-		<script src="/~team1/my/js/restarurant.js"></script><!--레스토랑 스크립트-->
-		<script src="/~team1/my/js/contect.js"></script><!--문의사항 스크립트-->
-        <script src="/~team1/my/vendors/lightbox/simpleLightbox.min.js"></script>
-        <script src="/~team1/my/js/custom.js"></script>
+	<script src="/my/js/detail_date.js"></script><!--디테일 room 스크립트-->
+	<script src="/my/js/restarurant.js"></script><!--레스토랑 스크립트-->
+	<script src="/my/js/contect.js"></script><!--문의사항 스크립트-->
+        <script src="/my/vendors/lightbox/simpleLightbox.min.js"></script>
+        <script src="/my/js/custom.js"></script>
         <!--회원가입 달력--->
-		<script type="text/javascript" src="http://momentjs.com/downloads/moment-with-locales.min.js"></script>
-		<script type="text/javascript" src="/~team1/my/js/bootstrap-material-datetimepicker.js"></script>
+	<script type="text/javascript" src="http://momentjs.com/downloads/moment-with-locales.min.js"></script>
+	<script type="text/javascript" src="/my/js/bootstrap-material-datetimepicker.js"></script>
 
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/0.5.10/js/ripples.min.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/0.5.10/js/material.min.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/locale/ko.min.js"></script>
-		<script>
-			$('#entranceDate').bootstrapMaterialDatePicker({
-				lang:'ko',
-				weekStart : 0,
-				cancelText: '취소',
-				okText: '확인',
-				relativeTime:'L',
-				time: false,
-				maxDate : new Date()
-			});
-			$('#editDate').bootstrapMaterialDatePicker({
-				lang:'ko',
-				weekStart : 0,
-				cancelText: '취소',
-				okText: '확인',
-				relativeTime:'L',
-				time: false,
-				maxDate : new Date()
-			});
-		</script>
-		<!--회원가입 달력--->
-
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/0.5.10/js/ripples.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/0.5.10/js/material.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/locale/ko.min.js"></script>
+	<script>
+		$('#entranceDate').bootstrapMaterialDatePicker({
+			lang:'ko',
+			weekStart : 0,
+			cancelText: '취소',
+			okText: '확인',
+			relativeTime:'L',
+			time: false,
+			maxDate : new Date()
+		});
+		$('#editDate').bootstrapMaterialDatePicker({
+			lang:'ko',
+			weekStart : 0,
+			cancelText: '취소',
+			okText: '확인',
+			relativeTime:'L',
+			time: false,
+			maxDate : new Date()
+		});
+	</script>
+	<!--회원가입 달력--->
         <script src="http://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
         <!-- datepicker 한국어로 -->
         <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/i18n/datepicker-ko.js"></script>
