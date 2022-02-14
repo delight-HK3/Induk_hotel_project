1f872e1708dc394f8d4208dd14ffafa7b0dfe8c9
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-02-15 08:00:06 +0900

    Update main_join.php
    img, script의 링크를 수정하고 코드 정리 작업을 했습니다.

--- view/main_join.php
+++ view/main_join.php
@@ -2,15 +2,15 @@
         <section class="banner_area">
             <div class="join_table d_flex align-items-center">
             	<div class="overlay bg-sign" data-stellar-ratio="0.9" data-stellar-vertical-offset="0" data-background=""></div>
-				<div class="container">
-					<div class="banner_content text-center animate__animated animate__fadeIn" >
-						<br><br>
-						<img style="width:650px; height:300px" src="/~team1/my/img/Hotel_name.png"><br><br>
-                        <h3 style="color:white; ">Induk Hotel의 패밀리가 되신것을 환영합니다.</h3><br>
-						<a href="/~team1/main" class="btn theme_btn button_hover">Main page</a>
-					</div>
-				</div>
+			<div class="container">
+				<div class="banner_content text-center animate__animated animate__fadeIn" >
+				<br><br>
+				<img style="width:650px; height:300px" src="/my/img/Hotel_name.png"><br><br>
+                        	<h3 style="color:white; ">Induk Hotel의 패밀리가 되신것을 환영합니다.</h3><br>
+				<a href="/main" class="btn theme_btn button_hover">Main page</a>
+			</div>
+		</div>
             </div>   
         </section>
         <!--================Banner Area =================-->
-		<script src="/~team1/my/js/jquery-3.5.1.min.js"></script>
+	<script src="/my/js/jquery-3.5.1.min.js"></script>
