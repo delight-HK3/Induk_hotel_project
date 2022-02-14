b3329733c66b1d26bf8ee079ea391f36d3a51105
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-02-15 07:48:50 +0900

    Update info_view.php
    script, img, a 태그의 경로를 수정하고 코드 정리 작업을 했습니다.

--- view/info_view.php
+++ view/info_view.php
@@ -1,129 +1,106 @@
 <!--===== footer 연락처 화면 =====-->
 
 <!--================Breadcrumb Area =================-->
-        <section class="breadcrumb_area">
-            <div class="overlay bg-parallax" data-stellar-ratio="0.8" data-stellar-vertical-offset="0" data-background=""></div>
-            <div class="container">
-               <div class="page-cover text-center">
-                 <!--     <h2 class="page-cover-tittle">고객 문의</h2>
-                    <ol class="breadcrumb">
-                        <li><a href="index.html">Home</a></li>
-                        <li class="active">Contact Us</li>
-                    </ol> -->
-					<img src="/~team1/my/img/Hotel_name.png">
-                </div>
-				
-            </div>
-        </section>
+<section class="breadcrumb_area">
+	<div class="overlay bg-parallax" data-stellar-ratio="0.8" data-stellar-vertical-offset="0" data-background=""></div>
+        <div class="container">
+        	<div class="page-cover text-center">
+			<img src="/my/img/Hotel_name.png">
+                </div>		
+        </div>
+</section>
 <!--================Breadcrumb Area =================-->
-
-
 <section class="information_gap">
     <div class="container">
-	
-		<div style="text-align:center">
-		<br>
-			<span style="color:black"><h2>인덕호텔 서울</h2></span>
-		</div>
-
-
-	<div class="container">
-	<hr><br>
-        <div class="row">
-               <div class="col-lg-3  col-md-6 col-sm-6">
-                    <div class="single-footer-widget">	
-						<span style="color:black"><h3>예약문의</h3></span>      
-					</div>
-				</div>
-			<div class="col-lg-3  col-md-6 col-sm-6">
-                <div class="single-footer-widget">
-					<span style="color:black"><h5>인덕호텔</h5> </span>
-						<p>TEL. 82-2-2951-7100</p>
-						<p>E-MAIL. comso@induk.net</p>
-		<br>
-                    </div>
-				</div>
-			</div>
-
-
-			<hr><br>
-	
-
-<!--========== 다이닝 연락처 시작 ==========-->
-	<div class="row">
-		<div class="col-lg-3  col-md-6 col-sm-6">
-			<div class="single-footer-widget">	
-				<span style="color:black"><h3>다이닝</h3></span>      
-			</div>
+        <div style="text-align:center">
+            <br>
+            <span style="color:black"><h2>인덕호텔 서울</h2></span>
         </div>
-			<div class="col-lg-3  col-md-6 col-sm-6">
-                <div class="single-footer-widget">
-					<span style="color:black"><h5>화련(花戀)</h5></span>
-						<p>TEL. 82-2-2952-7200</p>
-						<p>FAX. +82-2-102-9923</p>
-		<br>
-                </div>
-			</div>
 
-		<div class="col-lg-3  col-md-6 col-sm-6">
-			<div class="single-footer-widget">
-				<span style="color:black"><h5>Rouen</h5></span>
-					<p>TEL. 82-2-2952-7200</p>
-					<p>FAX. +82-2-101-9923</p>
-		<br>
-            </div>
-		</div>
-			
-	</div>
-</div>
-<!--========== 다이닝 연락처 끝 ==========-->
-
-<hr><br>
-
-<!--========== 부대시설 연락처 ==========-->
-	<div class="row">
-        <div class="col-lg-3  col-md-6 col-sm-6">
-            <div class="single-footer-widget">	
-				<span style="color:black"><h3>부대시설</h3></span>      
-			</div>
-		</div>
-
-		<div class="col-lg-3  col-md-6 col-sm-6">
-                <div class="single-footer-widget">
-					<span style="color:black"><h5>피트니스센터</h5></span>
-						<p>TEL. 82-2-2953-7310 </p>
-		<br>
-				<span style="color:black"><h5>더 라운지(Lounge)</h5> </span>
-						<p>TEL. 82-2-2953-7320 </p>
-                </div>
-			</div>
-
-			<div class="col-lg-3  col-md-6 col-sm-6">
-				<div class="single-footer-widget">
-					<span style="color:black"><h5>호텔사우나(SPA)</h5></span>
-						<p>TEL. 82-2-2953-7330 </p>
-		<br>
+        <div class="container">
+            <hr><br>
+                <div class="row">
+                    <div class="col-lg-3  col-md-6 col-sm-6">
+                        <div class="single-footer-widget">	
+                            <span style="color:black"><h3>예약문의</h3></span>      
+                        </div>
                     </div>
-				</div>
-
-			<div class="col-lg-3  col-md-6 col-sm-6">
+                    <div class="col-lg-3  col-md-6 col-sm-6">
                         <div class="single-footer-widget">
-						<span style="color:black"><h5>호텔수영장(Pool)</h5></span>
-						<p>TEL. 82-2-2953-7340 </p>
-		<br>
+                            <span style="color:black"><h5>인덕호텔</h5> </span>
+                                <p>TEL. 82-2-2951-7100</p>
+                                <p>E-MAIL. comso@induk.net</p>
+                            <br>
+                        </div>
+                    </div>
                 </div>
-	</div>
-</div>
-<hr><br>
+            <hr><br>
+            <!--========== 다이닝 연락처 시작 ==========-->
+            <div class="row">
+                <div class="col-lg-3  col-md-6 col-sm-6">
+                    <div class="single-footer-widget">	
+                        <span style="color:black"><h3>다이닝</h3></span>      
+                    </div>
+                </div>
+                <div class="col-lg-3  col-md-6 col-sm-6">
+                    <div class="single-footer-widget">
+                        <span style="color:black"><h5>화련(花戀)</h5></span>
+                        <p>TEL. 82-2-2952-7200</p>
+                        <p>FAX. +82-2-102-9923</p>
+                        <br>
+                    </div>
+                </div>
+                <div class="col-lg-3  col-md-6 col-sm-6">
+                    <div class="single-footer-widget">
+                        <span style="color:black"><h5>Rouen</h5></span>
+                        <p>TEL. 82-2-2952-7200</p>
+                        <p>FAX. +82-2-101-9923</p>
+                        <br>
+                    </div>
+                </div>		
+            </div>
+        </div>
+        <!--========== 다이닝 연락처 끝 ==========-->
 
-	<div class="button-group-area mt-40" align="right">
-		<a href="/~team1/about/contactindex" class="genric-btn primary e-large">문의하기</a>
-		<a href="/~team1/main" class="genric-btn danger-border e-large">홈으로</a>
-	</div>
+        <hr><br>
 
-		</div>
-	</div>
-</div>
-<br>
+        <!--========== 부대시설 연락처 ==========-->
+        <div class="row">
+                <div class="col-lg-3  col-md-6 col-sm-6">
+                        <div class="single-footer-widget">	
+                    <span style="color:black"><h3>부대시설</h3></span>      
+                    </div>
+                </div>
+                <div class="col-lg-3  col-md-6 col-sm-6">
+                            <div class="single-footer-widget">
+                        <span style="color:black"><h5>피트니스센터</h5></span>
+                        <p>TEL. 82-2-2953-7310 </p>
+                        <br>
+                        <span style="color:black"><h5>더 라운지(Lounge)</h5> </span>
+                        <p>TEL. 82-2-2953-7320 </p>
+                            </div>
+                </div>
+                <div class="col-lg-3  col-md-6 col-sm-6">
+                    <div class="single-footer-widget">
+                        <span style="color:black"><h5>호텔사우나(SPA)</h5></span>
+                        <p>TEL. 82-2-2953-7330 </p>
+                        <br>
+                                </div>
+                </div>
+                <div class="col-lg-3  col-md-6 col-sm-6">
+                                <div class="single-footer-widget">
+                        <span style="color:black"><h5>호텔수영장(Pool)</h5></span>
+                        <p>TEL. 82-2-2953-7340 </p>
+                        <br>
+                            </div>
+                </div>
+        </div>
+        <hr><br>
+            <div class="button-group-area mt-40" align="right">
+                <a href="/about/contactindex" class="genric-btn primary e-large">문의하기</a>
+                <a href="/main" class="genric-btn danger-border e-large">홈으로</a>
+            </div>
+    	</div>
+    <br>
 </section>
-<script src="/~team1/my/js/jquery-3.5.1.min.js"></script>
+<script src="/my/js/jquery-3.5.1.min.js"></script>
