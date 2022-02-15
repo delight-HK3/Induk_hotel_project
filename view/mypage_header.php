b45d2c780ed3561fe69017ce8daec783c7ffc451
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-02-16 07:48:47 +0900

    Update mypage_header.php
    css의 링크를 수정 했습니다.

--- view/mypage_header.php
+++ view/mypage_header.php
@@ -4,23 +4,22 @@
         <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="icon" href="/~team1/my/img/main_Logo.png" type="image/png">
+        <link rel="icon" href="/my/img/main_Logo.png" type="image/png">
         <title>INDUK Hotel</title>
         <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="/~team1/my/css/bootstrap.css">
-        <link rel="stylesheet" href="/~team1/my/vendors/linericon/style.css">
-        <link rel="stylesheet" href="/~team1/my/css/font-awesome.min.css">
-        <link rel="stylesheet" href="/~team1/my/vendors/bootstrap-datepicker/bootstrap-datetimepicker.min.css">
-        <link rel="stylesheet" href="/~team1/my/vendors/nice-select/css/nice-select.css">
-        <link rel="stylesheet" href="/~team1/my/vendors/owl-carousel/owl.carousel.min.css">
+        <link rel="stylesheet" href="/my/css/bootstrap.css">
+        <link rel="stylesheet" href="/my/vendors/linericon/style.css">
+        <link rel="stylesheet" href="/my/css/font-awesome.min.css">
+        <link rel="stylesheet" href="/my/vendors/bootstrap-datepicker/bootstrap-datetimepicker.min.css">
+        <link rel="stylesheet" href="/my/vendors/nice-select/css/nice-select.css">
+        <link rel="stylesheet" href="/my/vendors/owl-carousel/owl.carousel.min.css">
         <!-- main css -->
-        <link rel="stylesheet" href="/~team1/my/css/style.css">
+        <link rel="stylesheet" href="/my/css/style.css">
       
-		<!-- font css -->
-		<link rel="preconnect" href="https://fonts.googleapis.com">
-		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-		<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500&display=swap" rel="stylesheet">
-		
+	<!-- font css -->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500&display=swap" rel="stylesheet">
     </head>
     <body>	
         
