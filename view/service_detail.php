e01718deab3e2020d76eaea3a77b9f28a012e967
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-02-17 08:04:49 +0900

    Update service_detail.php
    script, form, img태그의 링크를 수정하고 코드 작업을 했습니다.

--- view/service_detail.php
+++ view/service_detail.php
@@ -1,9 +1,9 @@
 <section class="banner_area">
 	<div class="serveervation_table d_flex align-items-center">
-        <div class="overlay" style="background: url('/~team1/my/img/service/<?php echo($service->image_serve1);?>') no-repeat scroll center 0/cover; opacity: 0.5; " data-stellar-ratio="0.9" data-stellar-vertical-offset="0" data-background=""></div>
+        	<div class="overlay" style="background: url('/my/img/service/<?php echo($service->image_serve1);?>') no-repeat scroll center 0/cover; opacity: 0.5; " data-stellar-ratio="0.9" data-stellar-vertical-offset="0" data-background=""></div>
 		<div class="container">
 			<div class="banner_content text-center"><br><br><br><br><br><br><br><br>
-				<img src="/~team1/my/img/Hotel_name.png" class="animated fadeInDown" style="width:580px; height:266px"><br><br><br>
+				<img src="/my/img/Hotel_name.png" class="animated fadeInDown" style="width:580px; height:266px"><br><br><br>
 				<div class="animated fadeInUp">
 					<center><font class="animated fadeInUp" style="font-family: 'Noto Sans KR', sans-serif; font-size:40px; color:white;">Service</font>
 					<br><br><br>
@@ -11,64 +11,62 @@
 				</div>
 			</div>
 		</div>
-    </div> 
+    	</div> 
 </section>
 
 <section class="banner_area">
-
 	<section class="accomodation_area section_gap">
-        <div class="container">
+        	<div class="container">
 			<div style="text-align:center">
-                <font style="font-family: 'Noto Sans KR', sans-serif; font-size:35px; color:black; font-weight: 600; "><?php echo($service->name);?></font>
+                		<font style="font-family: 'Noto Sans KR', sans-serif; font-size:35px; color:black; font-weight: 600; "><?php echo($service->name);?></font>
 				<br><br><hr>
-            </div>
-				<!--===== 부대시설 사진 슬라이드 =====-->
-                    <div data-aos="zoom-in" data-aos-duration="3000" class = "text-center">       
-                        <div id="<?php echo($service->no);?>" class="carousel slide carousel-fade" data-interval = "false">
-                            <ol class="carousel-indicators">
-                                <li data-target="#<?php echo($service->no);?>" data-slide-to="0" class="active"></li>
-                                <li data-target="#<?php echo($service->no);?>" data-slide-to="1"></li>
-                                <li data-target="#<?php echo($service->no);?>" data-slide-to="2"></li>
-								<li data-target="#<?php echo($service->no);?>" data-slide-to="3"></li> 
-								<li data-target="#<?php echo($service->no);?>" data-slide-to="4"></li> 								
-                            </ol>
-                            <div class="carousel-inner">
-                                <div class="carousel-item active">
-                                    <img src="/~team1/my/img/service/<?php echo($service->image_serve1);?>" class="d-block w-100"  height="700" alt="1">
-                                </div>
-                                <div class="carousel-item">
-                                    <img src="/~team1/my/img/service/<?php echo($service->image_serve2);?>" class="d-block w-100"  height="700" alt="2">
-                                </div>
-                                <div class="carousel-item">
-                                    <img src="/~team1/my/img/service/<?php echo($service->image_serve3);?>" class="d-block w-100"  height="700" alt="3">
-                                </div>
-                                <div class="carousel-item">
-                                    <img src="/~team1/my/img/service/<?php echo($service->image_serve4);?>" class="d-block w-100"  height="700" alt="3">
-                                </div>
-                                <div class="carousel-item">
-                                    <img src="/~team1/my/img/service/<?php echo($service->image_serve5);?>" class="d-block w-100"  height="700" alt="3">
-                                </div>								
-                            </div>
-                            <a class="carousel-control-prev" href="#<?php echo($service->no);?>" role="button" data-slide="prev">
-                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span class="sr-only">Previous</span>
-                            </a>
-                            <a class="carousel-control-next" href="#<?php echo($service->no);?>" role="button" data-slide="next">
-                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span class="sr-only">Next</span>
-                            </a>
-                        </div>                  
-                    </div><hr><br>
+            		</div>
+			<!--===== 부대시설 사진 슬라이드 =====-->
+                    	<div data-aos="zoom-in" data-aos-duration="3000" class = "text-center">       
+                        	<div id="<?php echo($service->no);?>" class="carousel slide carousel-fade" data-interval = "false">
+				    	<ol class="carousel-indicators">
+						<li data-target="#<?php echo($service->no);?>" data-slide-to="0" class="active"></li>
+						<li data-target="#<?php echo($service->no);?>" data-slide-to="1"></li>
+						<li data-target="#<?php echo($service->no);?>" data-slide-to="2"></li>
+						<li data-target="#<?php echo($service->no);?>" data-slide-to="3"></li> 
+						<li data-target="#<?php echo($service->no);?>" data-slide-to="4"></li> 								
+				    	</ol>
+                            		<div class="carousel-inner">
+						<div class="carousel-item active">
+						    <img src="/my/img/service/<?php echo($service->image_serve1);?>" class="d-block w-100"  height="700" alt="1">
+						</div>
+						<div class="carousel-item">
+						    <img src="/my/img/service/<?php echo($service->image_serve2);?>" class="d-block w-100"  height="700" alt="2">
+						</div>
+						<div class="carousel-item">
+						    <img src="/my/img/service/<?php echo($service->image_serve3);?>" class="d-block w-100"  height="700" alt="3">
+						</div>
+						<div class="carousel-item">
+						    <img src="/my/img/service/<?php echo($service->image_serve4);?>" class="d-block w-100"  height="700" alt="3">
+						</div>
+						<div class="carousel-item">
+						    <img src="/my/img/service/<?php echo($service->image_serve5);?>" class="d-block w-100"  height="700" alt="3">
+						</div>								
+                            		</div>
+				    <a class="carousel-control-prev" href="#<?php echo($service->no);?>" role="button" data-slide="prev">
+					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					<span class="sr-only">Previous</span>
+				    </a>
+				    <a class="carousel-control-next" href="#<?php echo($service->no);?>" role="button" data-slide="next">
+					<span class="carousel-control-next-icon" aria-hidden="true"></span>
+					<span class="sr-only">Next</span>
+				    </a>
+			</div>                  
+		</div><hr><br>
                 <!--===== 부대시설 사진 슬라이드 =====-->
-
 				<!--===== 부대시설 설명 =====-->
-					<div style="text-align:center" >
-						<span style="color:black">
-							<h3>시설 소개</h3>
-						</span>
-						<font style="font-family: 'Noto Sans KR', sans-serif; font-size:16px"><?php echo($service->Explanation);?></font>
-					</div><br>
-					<hr><br>
+				<div style="text-align:center" >
+					<span style="color:black">
+						<h3>시설 소개</h3>
+					</span>
+					<font style="font-family: 'Noto Sans KR', sans-serif; font-size:16px"><?php echo($service->Explanation);?></font>
+				</div><br>
+				<hr><br>
 				<!--===== 부대시설 설명 =====-->
 
 				<!--===== 부대시설 사용 설명 =====-->				
@@ -99,32 +97,29 @@
 			<!--===== 객실개요 =====-->
 			<br><hr><br>
 			<!--===== 특별서비스 =====-->
-				<div class="container">
-					<div class="row" id="reple">
-						<div class="col-lg-3">
-							<div class="single-footer-widget">
-								<span style="color:black"><h3>추가정보</h3> </span>
-								<br>
-							</div>
-						</div>
-						<div class = "col-lg-9">
-
-							<li style="font-family: 'Noto Sans KR', sans-serif; font-size:15px; margin-top:5px; color:blue">정부의 사회적 거리두기 지침에 따라 영업 시간이 변동될 수 있습니다.</li>
-							<li style="font-family: 'Noto Sans KR', sans-serif; font-size:15px; margin-top:5px"><?php echo($service->bego);?></li>
-							<li style="font-family: 'Noto Sans KR', sans-serif; font-size:15px; margin-top:5px"><?php echo($service->insertbigo);?></li>
+			<div class="container">
+				<div class="row" id="reple">
+					<div class="col-lg-3">
+						<div class="single-footer-widget">
+							<span style="color:black"><h3>추가정보</h3> </span>
 							<br>
-							<li style="font-family: 'Noto Sans KR', sans-serif; font-size:17px; margin-top:5px">백신패스제 운영</li>
-								<ul style="list-style-type: none;">
-									<li style="font-family: 'Noto Sans KR', sans-serif; font-size:15px; margin-top:5px">- 시행 일자: 2021.11.15 부터</li>
-									<li style="font-family: 'Noto Sans KR', sans-serif; font-size:15px; margin-top:5px">- 19세 이상은 백신2차 접종 후 15일차부터 이용이 가능합니다.</li>
-									<li style="font-family: 'Noto Sans KR', sans-serif; font-size:15px; margin-top:5px">- 미접종자이신 분은 48시간 이내 PCR 음성확인서를 필수로 지참해 주시길 바랍니다.</li>
-									<li style="font-family: 'Noto Sans KR', sans-serif; font-size:15px; margin-top:5px">- 완치자 또는 백신 접종 예외로 인정되는 경우 보건소 확인증을 필수로 지참해 주시길 바랍니다.</li>
-								</ul>
-							
-
 						</div>
 					</div>
-				</div>	
+					<div class = "col-lg-9">
+						<li style="font-family: 'Noto Sans KR', sans-serif; font-size:15px; margin-top:5px; color:blue">정부의 사회적 거리두기 지침에 따라 영업 시간이 변동될 수 있습니다.</li>
+						<li style="font-family: 'Noto Sans KR', sans-serif; font-size:15px; margin-top:5px"><?php echo($service->bego);?></li>
+						<li style="font-family: 'Noto Sans KR', sans-serif; font-size:15px; margin-top:5px"><?php echo($service->insertbigo);?></li>
+						<br>
+						<li style="font-family: 'Noto Sans KR', sans-serif; font-size:17px; margin-top:5px">백신패스제 운영</li>
+						<ul style="list-style-type: none;">
+							<li style="font-family: 'Noto Sans KR', sans-serif; font-size:15px; margin-top:5px">- 시행 일자: 2021.11.15 부터</li>
+							<li style="font-family: 'Noto Sans KR', sans-serif; font-size:15px; margin-top:5px">- 19세 이상은 백신2차 접종 후 15일차부터 이용이 가능합니다.</li>
+							<li style="font-family: 'Noto Sans KR', sans-serif; font-size:15px; margin-top:5px">- 미접종자이신 분은 48시간 이내 PCR 음성확인서를 필수로 지참해 주시길 바랍니다.</li>
+							<li style="font-family: 'Noto Sans KR', sans-serif; font-size:15px; margin-top:5px">- 완치자 또는 백신 접종 예외로 인정되는 경우 보건소 확인증을 필수로 지참해 주시길 바랍니다.</li>
+						</ul>
+					</div>
+				</div>
+			</div>	
 			<!--===== 특별서비스 =====-->
 			<hr>
 			<br><br><br><br>
@@ -145,7 +140,7 @@
 						foreach($reple as $row)
 						{
 					?>		
-							<form action="/~team1/service/reple_delete/no/<?php echo($row->no)?>/service_no/<?php echo($service->no);?>" method="post" >
+							<form action="/service/reple_delete/no/<?php echo($row->no)?>/service_no/<?php echo($service->no);?>" method="post" >
 							<input type="hidden" value="<?php echo($row->reple_rank)?>">
 							<font style="padding-top:9px; padding-bottom:9px; font-family: 'Noto Sans KR', sans-serif; font-size:25px;"><?php echo($row->reple_id);?></font>
 							<?php
@@ -167,55 +162,55 @@
 								<?php
 									if($row->reple_rank == 5){
 								?>
-										<img src="/~team1/my/img/star-on.png">
-										<img src="/~team1/my/img/star-on.png">
-										<img src="/~team1/my/img/star-on.png">
-										<img src="/~team1/my/img/star-on.png">
-										<img src="/~team1/my/img/star-on.png">
+										<img src="/my/img/star-on.png">
+										<img src="/my/img/star-on.png">
+										<img src="/my/img/star-on.png">
+										<img src="/my/img/star-on.png">
+										<img src="/my/img/star-on.png">
 								<?php
 									}
 								?>
 								<?php
 									if($row->reple_rank == 4){
 								?>
-										<img src="/~team1/my/img/star-on.png">
-										<img src="/~team1/my/img/star-on.png">
-										<img src="/~team1/my/img/star-on.png">
-										<img src="/~team1/my/img/star-on.png">
-										<img src="/~team1/my/img/star-off.png">
+										<img src="/my/img/star-on.png">
+										<img src="/my/img/star-on.png">
+										<img src="/my/img/star-on.png">
+										<img src="/my/img/star-on.png">
+										<img src="/my/img/star-off.png">
 								<?php
 									}
 								?>
 								<?php
 									if($row->reple_rank == 3){
 								?>
-										<img src="/~team1/my/img/star-on.png">
-										<img src="/~team1/my/img/star-on.png">
-										<img src="/~team1/my/img/star-on.png">
-										<img src="/~team1/my/img/star-off.png">
-										<img src="/~team1/my/img/star-off.png">
+										<img src="/my/img/star-on.png">
+										<img src="/my/img/star-on.png">
+										<img src="/my/img/star-on.png">
+										<img src="/my/img/star-off.png">
+										<img src="/my/img/star-off.png">
 								<?php
 									}
 								?>
 								<?php
 									if($row->reple_rank == 2){
 								?>
-										<img src="/~team1/my/img/star-on.png">
-										<img src="/~team1/my/img/star-on.png">
-										<img src="/~team1/my/img/star-off.png">
-										<img src="/~team1/my/img/star-off.png">
-										<img src="/~team1/my/img/star-off.png">
+										<img src="/my/img/star-on.png">
+										<img src="/my/img/star-on.png">
+										<img src="/my/img/star-off.png">
+										<img src="/my/img/star-off.png">
+										<img src="/my/img/star-off.png">
 								<?php
 									}
 								?>
 								<?php
 									if($row->reple_rank == 1){
 								?>
-										<img src="/~team1/my/img/star-on.png">
-										<img src="/~team1/my/img/star-off.png">
-										<img src="/~team1/my/img/star-off.png">
-										<img src="/~team1/my/img/star-off.png">
-										<img src="/~team1/my/img/star-off.png">
+										<img src="/my/img/star-on.png">
+										<img src="/my/img/star-off.png">
+										<img src="/my/img/star-off.png">
+										<img src="/my/img/star-off.png">
+										<img src="/my/img/star-off.png">
 								<?php
 									}
 								?><br><br>
@@ -240,7 +235,7 @@
 					<span aria-hidden="true">&times;</span>
 				</button>
 			</div><br>
-			<form method="post" action="/~team1/service/reple/no/<?php echo($service->no)?>?>" onsubmit="return replecheck()">
+			<form method="post" action="/service/reple/no/<?php echo($service->no)?>?>" onsubmit="return replecheck()">
 			<h4 class="modal-title" style="text-align: center; margin-top:10px; margin-bottom:10px" id="exampleModelLabel">이용후기</h4><br>
 				<div class="modal-body bg-light" style="width:400px;">
 					<div style="padding:15px">
@@ -293,7 +288,7 @@
 	</div>
 </div>
 
-<script src="/~team1/my/js/jquery-3.5.1.min.js"></script>
+<script src="/my/js/jquery-3.5.1.min.js"></script>
 
 <script>
 	$(document).ready(function(){
