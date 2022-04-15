bf75d2e30443c6a3713a0325940329d2be897a47
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-04-15 13:07:50 +0900

    Update Adminroomgubun.php

--- controller/Adminroomgubun.php
+++ controller/Adminroomgubun.php
@@ -1,4 +1,6 @@
-<?
+<?php
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+
 class Adminroomgubun extends CI_Controller {
     function __construct(){
         parent::__construct();
